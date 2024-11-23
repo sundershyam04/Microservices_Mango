8c95875e69b8cfceb670112c98269fd442427074
Shyam sunder@04 <sundershyam6161@gmail.com>
2024-11-24 00:27:11 +0530

    Add Custom Attribute to Product Image

---
+++ Mango.Web/Utility/AllowedExtensionsAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Web.Utility
+{
+    public class AllowedExtensionsAttribute : ValidationAttribute
+    {
+        private readonly string[] _allowedExtensions;
+        public AllowedExtensionsAttribute(string[] extensions) 
+        {
+            _allowedExtensions = extensions;
+        }
+
+        // [value]: FormFile type (Image)
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            var file = value as IFormFile;
+            if (file != null)
+            {
+                var extension = Path.GetExtension(file.FileName);
+                if (!_allowedExtensions.Contains(extension))
+                {
+                    return new ValidationResult("This photo extension is not allowed");
+                }
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
